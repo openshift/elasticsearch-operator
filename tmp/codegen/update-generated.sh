e3a34e9327459705cbdb6fb1142df4fc398089d6
Anton Sherkhonov <sherkhonov@gmail.com>
2018-05-14 23:55:02 -0400

    some boilerplate code from operator-sdk

---
+++ tmp/codegen/update-generated.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+DOCKER_REPO_ROOT="/go/src/github.com/t0ffel/elasticsearch-operator"
+IMAGE=${IMAGE:-"gcr.io/coreos-k8s-scale-testing/codegen:1.9.3"}
+
+docker run  --rm \
+  -v "$PWD":"$DOCKER_REPO_ROOT":Z \
+  -w "$DOCKER_REPO_ROOT" \
+  "$IMAGE" \
+  "/go/src/k8s.io/code-generator/generate-groups.sh"  \
+  "deepcopy" \
+  "github.com/t0ffel/elasticsearch-operator/pkg/generated" \
+  "github.com/t0ffel/elasticsearch-operator/pkg/apis" \
+  "elasticsearch:v1alpha1" \
+  --go-header-file "./tmp/codegen/boilerplate.go.txt" \
+  $@
