13d2b642e16b18c8161a9ad3787f6e6f07c411c9
Anton Sherkhonov <asherkho@redhat.com>
2018-05-14 20:46:20 -0400

    Initial commit adding generated files

---
+++ pkg/apis/elasticsearch/v1alpha1/types.go
@@ -0,0 +1,44 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+        "k8s.io/api/core/v1"
+)
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type ElasticsearchList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+	Items           []Elasticsearch `json:"items"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type Elasticsearch struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+	Spec              ElasticsearchSpec   `json:"spec"`
+	Status            ElasticsearchStatus `json:"status,omitempty"`
+}
+
+type ElasticsearchNode struct {
+	NodeRole          string `json:"nodeRole"`
+	Replicas          int32 `json:"replicas"`
+	Resources         v1.ResourceRequirements `json:"resources"`
+}
+
+type ElasticsearchNodeStatus struct {
+	PodName           string `json:"podName"`
+	Status            string `json:"status"`
+}
+
+
+type ElasticsearchSpec struct {
+	// Fill me
+        Nodes             []ElasticsearchNode `json:"nodes"`
+}
+type ElasticsearchStatus struct {
+	// Fill me
+        Nodes             []ElasticsearchNodeStatus `json:"nodes"`
+}
