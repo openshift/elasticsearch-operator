083a114813c7b6c7205bafa84563a3b1c346a9fd
t0ffel <sherkhonov@gmail.com>
2018-07-08 11:36:08 -0400

    Update operator-sdk version

--- tmp/codegen/update-generated.sh
+++ tmp/codegen/update-generated.sh
@@ -4,17 +4,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-DOCKER_REPO_ROOT="/go/src/github.com/ViaQ/elasticsearch-operator"
-IMAGE=${IMAGE:-"gcr.io/coreos-k8s-scale-testing/codegen:1.9.3"}
-
-docker run --rm \
-  -v "$PWD":"$DOCKER_REPO_ROOT":Z \
-  -w "$DOCKER_REPO_ROOT" \
-  "$IMAGE" \
-  "/go/src/k8s.io/code-generator/generate-groups.sh"  \
-  "deepcopy" \
-  "github.com/ViaQ/elasticsearch-operator/pkg/generated" \
-  "github.com/ViaQ/elasticsearch-operator/pkg/apis" \
-  "elasticsearch:v1alpha1" \
-  --go-header-file "./tmp/codegen/boilerplate.go.txt" \
-  $@
+vendor/k8s.io/code-generator/generate-groups.sh \
+deepcopy \
+github.com/ViaQ/elasticsearch-operator/pkg/generated \
+github.com/ViaQ/elasticsearch-operator/pkg/apis \
+elasticsearch:v1alpha1 \
+--go-header-file "./tmp/codegen/boilerplate.go.txt"
