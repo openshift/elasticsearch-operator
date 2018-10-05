5ad6a3eeebbc30c53ef15375e070819e82e0d724
Josef Karasek <jkarasek@redhat.com>
2018-10-05 17:39:11 +0200

    LOG-193 move to openshift namespace

--- pkg/stub/handler.go
+++ pkg/stub/handler.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/ViaQ/elasticsearch-operator/pkg/apis/elasticsearch/v1alpha1"
-	"github.com/ViaQ/elasticsearch-operator/pkg/k8shandler"
+	"github.com/openshift/elasticsearch-operator/pkg/apis/elasticsearch/v1alpha1"
+	"github.com/openshift/elasticsearch-operator/pkg/k8shandler"
 	"github.com/sirupsen/logrus"
 
 	"github.com/operator-framework/operator-sdk/pkg/sdk"
