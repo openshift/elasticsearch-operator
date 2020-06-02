e0ac57e5d239b6691431edcec040d6789f56046f
Periklis Tsirakidis <periklis@redhat.com>
2020-06-02 09:25:46 +0200

    Bug 1842043: Stop reconciliation on ES CR not present

--- pkg/k8shandler/cr.go
+++ pkg/k8shandler/cr.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 
 	loggingv1 "github.com/openshift/elasticsearch-operator/pkg/apis/logging/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -13,11 +15,15 @@ func GetElasticsearchCR(c client.Client, ns string) (*loggingv1.Elasticsearch, e
 	opts := &client.ListOptions{Namespace: ns}
 
 	if err := c.List(context.TODO(), opts, esl); err != nil {
-		return nil, fmt.Errorf("failed to find elasticsearch instance in %q: %s", ns, err)
+		return nil, fmt.Errorf("failed to find elasticsearch instance in %q: %w", ns, err)
 	}
 
 	if len(esl.Items) == 0 {
-		return nil, fmt.Errorf("failed to find elasticsearch instance in %q: empty result set", ns)
+		gr := schema.GroupResource{
+			Group:    loggingv1.SchemeGroupVersion.Group,
+			Resource: "Elasticsearch",
+		}
+		return nil, errors.NewNotFound(gr, "elasticsearch")
 	}
 
 	return &esl.Items[0], nil
