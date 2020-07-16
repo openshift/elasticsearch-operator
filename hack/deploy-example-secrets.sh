ac05e56cb83321f0591c1068de46466efaf378cc
Eric Wolinetz <ewolinet@redhat.com>
2020-07-16 15:26:39 -0500

    simplifying dev kibana experience

--- hack/deploy-example-secrets.sh
+++ hack/deploy-example-secrets.sh
@@ -14,3 +14,14 @@ oc -n $namespace create secret generic elasticsearch  \
 	--from-file=/tmp/example-secrets/logging-es.key \
 	--from-file=/tmp/example-secrets/logging-es.crt
 
+oc -n $namespace delete secret kibana ||:
+oc -n $namespace create secret generic kibana  \
+	--from-file=ca=/tmp/example-secrets/ca.crt \
+	--from-file=key=/tmp/example-secrets/system.logging.kibana.key \
+	--from-file=cert=/tmp/example-secrets/system.logging.kibana.crt
+
+oc -n $namespace delete secret kibana-proxy ||:
+oc -n $namespace create secret generic kibana-proxy  \
+	--from-literal=session-secret=abcdefghijklmnopqrstuvwxyz123456 \
+	--from-file=server-key=/tmp/example-secrets/kibana-internal.key \
+	--from-file=server-cert=/tmp/example-secrets/kibana-internal.crt
