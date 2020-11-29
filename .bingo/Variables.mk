073c18ac395e15e8f68ed2315aa04f756684b1f9
Hui Kang <hkang@redhat.com>
2020-11-29 03:39:20 +0000

    Enable metrics with controller-runtime
    - use operator-sdk v1.2.0 which resolves the problem of generating
    service and serviceMonitor in previous version

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -63,11 +63,11 @@ $(KUSTOMIZE): .bingo/kustomize.mod
 	@echo "(re)installing $(GOBIN)/kustomize-v3.5.4"
 	@cd .bingo && $(GO) build -mod=mod -modfile=kustomize.mod -o=$(GOBIN)/kustomize-v3.5.4 "sigs.k8s.io/kustomize/kustomize/v3"
 
-OPERATOR_SDK := $(GOBIN)/operator-sdk-v1.1.0
+OPERATOR_SDK := $(GOBIN)/operator-sdk-v1.2.0
 $(OPERATOR_SDK): .bingo/operator-sdk.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/operator-sdk-v1.1.0"
-	@cd .bingo && $(GO) build -mod=mod -modfile=operator-sdk.mod -o=$(GOBIN)/operator-sdk-v1.1.0 "github.com/operator-framework/operator-sdk/cmd/operator-sdk"
+	@echo "(re)installing $(GOBIN)/operator-sdk-v1.2.0"
+	@cd .bingo && $(GO) build -mod=mod -modfile=operator-sdk.mod -o=$(GOBIN)/operator-sdk-v1.2.0 "github.com/operator-framework/operator-sdk/cmd/operator-sdk"
 
 OPM := $(GOBIN)/opm-v1.13.6
 $(OPM): .bingo/opm.mod
