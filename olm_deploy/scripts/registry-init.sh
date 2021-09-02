8706f1b772fbcf676dbe8c6a2438bf1516265889
Periklis Tsirakidis <periklis@redhat.com>
2021-09-02 14:18:20 +0300

    Fix olm deploy env naming for curator operand image

--- olm_deploy/scripts/registry-init.sh
+++ olm_deploy/scripts/registry-init.sh
@@ -12,7 +12,7 @@ IMAGE_ELASTICSEARCH6=${IMAGE_ELASTICSEARCH6:-quay.io/openshift-logging/elasticse
 IMAGE_ELASTICSEARCH_PROXY=${IMAGE_ELASTICSEARCH_PROXY:-quay.io/openshift-logging/elasticsearch-proxy:1.0}
 IMAGE_OAUTH_PROXY=${IMAGE_OAUTH_PROXY:-quay.io/openshift/origin-oauth-proxy:latest}
 IMAGE_LOGGING_KIBANA6=${IMAGE_LOGGING_KIBANA6:-quay.io/openshift-logging/kibana6:6.8.1}
-IMAGE_CURATOR5=${IMAGE_CURATOR5:-quay.io/openshift-logging/curator5:5.8.1}
+IMAGE_LOGGING_CURATOR5=${IMAGE_LOGGING_CURATOR5:-quay.io/openshift-logging/curator5:5.8.1}
 
 # update the manifest with the image built by ci
 sed -i "s,quay.io/openshift-logging/elasticsearch-operator:latest,${IMAGE_ELASTICSEARCH_OPERATOR}," /manifests/*clusterserviceversion.yaml
@@ -21,7 +21,7 @@ sed -i "s,quay.io/openshift-logging/elasticsearch6:6.8.1,${IMAGE_ELASTICSEARCH6}
 sed -i "s,quay.io/openshift-logging/elasticsearch-proxy:1.0,${IMAGE_ELASTICSEARCH_PROXY}," /manifests/*clusterserviceversion.yaml
 sed -i "s,quay.io/openshift/origin-oauth-proxy:latest,${IMAGE_OAUTH_PROXY}," /manifests/*clusterserviceversion.yaml
 sed -i "s,quay.io/openshift-logging/kibana6:6.8.1,${IMAGE_LOGGING_KIBANA6}," /manifests/*clusterserviceversion.yaml
-sed -i "s,quay.io/openshift-logging/curator5:5.8.1,${IMAGE_CURATOR5}," /manifests/*clusterserviceversion.yaml
+sed -i "s,quay.io/openshift-logging/curator5:5.8.1,${IMAGE_LOGGING_CURATOR5}," /manifests/*clusterserviceversion.yaml
 
 # update the manifest to pull always the operator image for non-CI environments
 if [ "${OPENSHIFT_CI:-false}" == "false" ] ; then
